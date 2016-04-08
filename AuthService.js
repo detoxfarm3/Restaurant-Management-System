4c02e4c13395ddc2b5894315a5be711311db4cde
codefacts <codefacts63@gmail.com>
2016-04-08 15:52:43 +0600

    Navbar Complete

---
+++ AuthService.js
@@ -0,0 +1,27 @@
+var Apis = require('./Apis');
+var Promise = require('bluebird');
+var ee = require('./EventEmitter');
+var Events = require('./Events');
+var $ = require('jquery');
+
+class AuthService {
+
+    login() {
+
+    }
+
+    logout() {
+
+    }
+
+    isLoggedIn() {
+
+    }
+
+    currentUser() {
+
+    }
+
+}
+
+module.exports = new AuthService();
